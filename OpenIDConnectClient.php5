jumbojett/OpenID-Connect-PHP

commit 19a23a84110095188c0d45492e56bd2d4bdebb43

Resolved "Unable to verify signature" by adding get_key_for_header from https://github.com/jumbojett/OpenID-Connect-PHP/pull/20

OpenIDConnectClient.php5
@@ -388,6 +388,22 @@ class OpenIDConnectClient
     }
 
     /**
+      * @param array $keys
+      * @param array $header
+      * @throws OpenIDConnectClientException
+      * @return object
+      */
+     private function get_key_for_header($keys, $header) {
+         foreach ($keys as $key) {
+             if ($key->alg == $header->alg && $key->kid == $header->kid) {
+                 return $key;
+             }
+         }
+         throw new OpenIDConnectClientException('Unable to find a key for (algorithm, kid):' . $header->alg . ', ' . $header->kid . ')');
+     }
+ 
+
+    /**
      * @param array $keys
      * @param string $alg
      * @throws OpenIDConnectClientException
@@ -451,7 +467,7 @@ class OpenIDConnectClient
         case 'RS512':
             $hashtype = 'sha' . substr($header->alg, 2);
             $verified = $this->verifyRSAJWTsignature($hashtype,
-                                                     $this->get_key_for_alg($jwks->keys, 'RSA'),
+                                                     $this->get_key_for_header($jwks->keys, $header),
                                                      $payload, $signature);
             break;
         default:

commit 1d310b54d9ab2f42b8fd95845186df6eaae51af0

Update OpenIDConnectClient.php5
Patch https://github.com/jumbojett/OpenID-Connect-PHP/commit/19a23a84110095188c0d45492e56bd2d4bdebb43 introduced a problem for our environment where $header->kid is undefined. The code dies on line 406 and is unable to even throw the exception on line 410 because of the missing field. I'm not sure if the proposed fix is the best solution, but it does may the code work again.

OpenIDConnectClient.php5
@@ -474,8 +474,12 @@ class OpenIDConnectClient
         case 'RS384':
         case 'RS512':
             $hashtype = 'sha' . substr($header->alg, 2);
-            $verified = $this->verifyRSAJWTsignature($hashtype,
-                                                     $this->get_key_for_header($jwks->keys, $header),
+            if (isset($header->kid)) {
+                $key = $this->get_key_for_header($jwks->keys, $header);
+            } else {
+                $key = $this->get_key_for_alg($jwks->keys, 'RSA');
+            }        
+            $verified = $this->verifyRSAJWTsignature($hashtype, $key,
                                                      $payload, $signature);
             break;
         default: