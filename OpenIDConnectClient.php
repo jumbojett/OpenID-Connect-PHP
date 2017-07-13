065717e03af9421ab6e01d3b0ab8d678ca628073
Chris Gray <christophermjgray@gmail.com>
2017-07-12 22:59:46 -0400

    Update OpenIDConnectClient.php
    Add check for header "HTTP_UPGRADE_INSECURE_REQUESTS". Needed when this code is behind a proxy doing SSL termination.  Tested on F5 BigIP 12.1.2.  F5 adds the header, which this uses to rewrite the request added "https" to the original "http".

--- OpenIDConnectClient.php
+++ OpenIDConnectClient.php
@@ -409,13 +409,13 @@ class OpenIDConnectClient
          * Support of 'ProxyReverse' configurations.
          */
 
-        if (isset($_SERVER["HTTP_UPGRADE_INSECURE_REQUESTS"]) && ($_SERVER['HTTP_UPGRADE_INSECURE_REQUESTS'] == 1)) {
-            $protocol = 'https';
-        } else {
-            $protocol = @$_SERVER['HTTP_X_FORWARDED_PROTO'] 
-                ?: @$_SERVER['REQUEST_SCHEME']
-                ?: ((isset($_SERVER["HTTPS"]) && $_SERVER["HTTPS"] == "on") ? "https" : "http");
-        }
+        if (isset($_SERVER["HTTP_UPGRADE_INSECURE_REQUESTS"]) && ($_SERVER['HTTP_UPGRADE_INSECURE_REQUESTS'] == 1)) { 
+            $protocol = 'https'; 
+        } else { 
+            $protocol = @$_SERVER['HTTP_X_FORWARDED_PROTO']
+                ?: @$_SERVER['REQUEST_SCHEME'] 
+                ?: ((isset($_SERVER["HTTPS"]) && $_SERVER["HTTPS"] == "on") ? "https" : "http"); 
+        } 
 
         $port = @intval($_SERVER['HTTP_X_FORWARDED_PORT'])
               ?: @intval($_SERVER["SERVER_PORT"])
